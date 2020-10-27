b4bd4bcb71c95946c6b9b408fbcfb48a98db8a1a
Chris Steigerwald <chrissteigerwald@Chriss-MBP.attlocal.net>
2020-10-27 08:01:16 -0700

    Basic blog finished

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe cum necessitatibus magnam magni quas officia asperiores culpa vero doloribus velit quo aperiam a laborum eum adipisci sint aspernatur placeat aliquid eaque accusamus rem, possimus deleniti similique. Ab quia rem tenetur doloremque magnam illo corrupti, quaerat mollitia placeat vel reprehenderit earum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nostrum, excepturi id mollitia molestias dolore aut reprehenderit cumque tempora quam recusandae quidem ex quas dolores nobis? Ab vero minus tenetur quibusdam similique sint eaque sit officiis doloribus impedit magnam veritatis ea pariatur autem ullam quam fugiat laboriosam, omnis eveniet! Quisquam!</p>
+    </>
+)
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = ( { articles }) => (
+    <>   
+        <h1>404: Page Not Found</h1>
+        
+    </>
+)
+
+export default NotFoundPage;
